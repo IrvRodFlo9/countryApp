b8a23ba22194f3926c749c72e47eeb438aaecd4b
IrvRodFlo9JU <irvrodflo9@gmail.com>
2023-12-01 06:54:28 -0600

    routing problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRouterModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
